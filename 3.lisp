d47e5a5880bdd013d80847df4142439b4b04735a
Noor Khan <noordotkhan@gmail.com>
2020-01-28 21:16:34 -0500

    More work

--- 3.lisp
+++ 3.lisp
@@ -6,8 +6,6 @@
 (defun make-cd (title artist rating ripped)
   (list :title title :artist artist :rating rating :ripped ripped))
 
-(defvar *db* nil)
-
 (defun add-record (cd) (push cd *db*))
 
 (add-record (make-cd "Roses" "Kathy Mattea" 7 t))
@@ -29,4 +27,22 @@
    (or (parse-integer (prompt-read "Rating") :junk-allowed t) 0)
    (y-or-n-p "Ripped [y/n]: ")))
 
+(defun add-cds ()
+  (loop (add-record (prompt-for-cd))
+     (if (not (y-or-n-p "Another? [y/n]: ")) (return))))
 
+(defun save-db (filename)
+  (with-open-file (out filename
+                   :direction :output
+                   :if-exists :supersede)
+    (with-standard-io-syntax
+      (print *db* out))))
+
+(defun load-db (filename)
+  (with-open-file (in filename
+		      :if-does-not-exist nil)
+    (with-standard-io-syntax
+      (setf *db* (read in)))))
+
+(defvar *filename* "my-cds.db")
+(load-db *filename*)
