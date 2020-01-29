7360f7559c1bf46f5043c8b6c5be93a346c70f82
Noor Khan <noordotkhan@gmail.com>
2020-01-28 19:00:48 -0500

    Chapter 3 part 1

---
+++ 3.lisp
@@ -0,0 +1,32 @@
+(defun hello-world ()
+  (format t "Hello, world!"))
+
+(hello-world)
+
+(defun make-cd (title artist rating ripped)
+  (list :title title :artist artist :rating rating :ripped ripped))
+
+(defvar *db* nil)
+
+(defun add-record (cd) (push cd *db*))
+
+(add-record (make-cd "Roses" "Kathy Mattea" 7 t))
+(add-record (make-cd "Fly" "Dixie Chicks" 8 t))
+(add-record (make-cd "Home" "Dixie Chicks" 9 t))
+
+(defun dump-db ()
+  (format t "~{~{~a:~10t~a~%~}~%~}" *db*))
+
+(defun prompt-read (prompt)
+  (format *query-io* "~a: " prompt)
+  (force-output *query-io*)
+  (read-line *query-io*))
+
+(defun prompt-for-cd ()
+  (make-cd
+   (prompt-read "Title")
+   (prompt-read "Artist")
+   (or (parse-integer (prompt-read "Rating") :junk-allowed t) 0)
+   (y-or-n-p "Ripped [y/n]: ")))
+
+
